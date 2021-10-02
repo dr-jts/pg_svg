6c3643fd797a8e2f7e611b9e490c0f77cb858014
Martin Davis <mtnclimb@gmail.com>
2021-10-02 10:55:17 -0700

    Change sierpinski-carpet to SVG

--- demo/fractal/sierpinski-carpet.sql
+++ demo/fractal/sierpinski-carpet.sql
@@ -1,6 +1,8 @@
 -- ---------------------------------
 -- Sierpinski carpet, as GEoJSON
 -- Parameter: level
+--
+-- psql -A -t -o sierpinski-carpet.svg  < sierpinski-carpet.sql
 -- ---------------------------------
 
 WITH RECURSIVE
@@ -24,5 +26,20 @@ sierpinski_carpet( geom ) AS (
   SELECT ST_MakePolygon( ST_ExteriorRing(ST_MakeEnvelope(0,0,1,1)),
     array_agg( ST_ExteriorRing(hole)) ) AS geom
   FROM holes
+),
+scale_geom( geom ) AS (
+  SELECT ST_Translate( ST_Scale(geom, 10, 10 ), 0, 23) AS geom
+  FROM sierpinski_carpet
+),
+--SELECT ST_AsGeoJSON( ST_Translate( ST_Scale(geom, 10, 10 ), 0, 23), 2 ) FROM sierpinski_carpet;
+shapes AS (
+  SELECT geom, svgShape( geom,
+        title => 'Sierpinski Carpet',
+        style => svgStyle( 'fill', '#88ff88',
+                           'stroke', '#000000', 'stroke-width', '0.1'
+            )
+    ) svg FROM scale_geom
 )
-SELECT ST_AsGeoJSON( ST_Translate( ST_Scale(geom, 10, 10 ), 0, 23), 2 ) FROM sierpinski_carpet;
+SELECT svgDoc( array_agg( svg ),
+            svgViewbox( ST_Expand( ST_Extent(geom), 5 ))
+  ) AS svg FROM shapes;
