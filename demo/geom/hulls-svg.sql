8741e5d414a882e72829cb1d4b13a045dc10262e
Martin Davis <mtnclimb@gmail.com>
2021-11-05 16:39:04 -0700

    Renaming

--- demo/geom/hulls-svg.sql
+++ demo/geom/hulls-svg.sql
@@ -2,7 +2,7 @@
 -- Demo of using SVG functions to display Convex and Concave Hulls
 -- Author: Martin Davis  2019
 
--- psql -At -o hull.svg  < hull-svg.sql
+-- psql -At -o hulls.svg  < hulls-svg.sql
 ------------------------------------------------------------------
 
 WITH input AS (
