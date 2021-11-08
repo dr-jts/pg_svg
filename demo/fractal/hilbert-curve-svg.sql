76852742cf6d07b4a7b19ba78f9a5840c325cb6b
Martin Davis <mtnclimb@gmail.com>
2021-11-08 14:14:10 -0800

    Improve hilbert SQL

--- demo/fractal/hilbert-curve-svg.sql
+++ demo/fractal/hilbert-curve-svg.sql
@@ -11,15 +11,14 @@
 WITH RECURSIVE lsystem AS (
   SELECT 'A' AS state, 0 AS iteration
   UNION ALL
-  SELECT replace(replace(replace(state, 'A', '-CF+AFA+FC-'), 'B', '+AF-BFB-FA+'), 'C', 'B'), iteration+1 AS iteration
-  FROM lsystem WHERE iteration < 5
+  SELECT replace(replace(replace(state, 'A', '-CF+AFA+FC-'), 'B', '+AF-BFB-FA+'), 'C', 'B'),
+          iteration + 1 AS iteration
+  FROM lsystem WHERE iteration < 5  -- Iteration parameter
 ),
-path AS (
-  SELECT replace(replace(state, 'A', ''), 'B', '') AS moves
+path AS ( SELECT replace(replace(state, 'A', ''), 'B', '') AS moves
   FROM (SELECT state FROM lsystem ORDER BY iteration DESC LIMIT 1) st
 ),
-pts AS (
-  SELECT moves AS moves, 1 AS index, ' ' AS dir,
+pts AS ( SELECT moves AS moves, 1 AS index, ' ' AS dir,
       0 AS xp, 0 AS yp, 0 AS x, 0 AS y, 1 AS dx, 0 AS dy, 0 AS len
     FROM path
   UNION ALL
@@ -31,18 +30,17 @@ pts AS (
       CASE substr(moves, index, 1) WHEN 'F' THEN 1 ELSE 0 END AS len
     FROM pts WHERE index <= length(moves)
 ),
-hilbert AS (
-  SELECT row_number() over() as id, ST_MakeLine( ST_MakePoint(xp, yp), ST_MakePoint(x, y)) geom
+seg AS (
+  SELECT row_number() over() as id, ST_MakeLine( ST_Point(xp, yp), ST_Point(x, y)) geom
   FROM pts WHERE xp <> x OR yp <> y
 ),
-svg AS (
-    SELECT geom, svgShape( geom,
+svg AS ( SELECT geom, svgShape( geom,
         style => svgStyle('stroke', svgHSL( 300*(id/ 1024.0 ), 100, 50))
       ) AS svg
-    FROM hilbert
+    FROM seg
 )
 SELECT svgDoc( array_agg( svg ),
-    viewbox => svgViewbox( ST_Expand( ST_Extent(geom), 5 )),
-    style => svgStyle('stroke-width', 0.5::text,
-        'stroke-linecap', 'round' ) ) AS svg
+          viewbox => svgViewbox( ST_Expand( ST_Extent(geom), 5 )),
+          style => svgStyle('stroke-width', '0.5', 'stroke-linecap', 'round' )
+    ) AS svg
   FROM svg;
