8c12feafb96ea993cb80673793831dafa780d6fe
Martin Davis <mtnclimb@gmail.com>
2021-11-08 14:03:06 -0800

    Improve hulls SQL

--- demo/geom/hulls-svg.sql
+++ demo/geom/hulls-svg.sql
@@ -8,31 +8,20 @@
 WITH data AS (
   SELECT 'MULTIPOINT ((178 80), (174 133), (66 151), (162 163), (205 139), (147 143), (29 157), (85 160), (79 129), (117 158), (110 158), (84 186), (134 57), (177 167), (178 107), (190 112), (58 65), (67 129), (68 103), (176 183), (51 103), (90 80), (48 136), (148 76))'::geometry geom
 ),
-mic AS ( SELECT (mic).center, (mic).radius FROM
-          (SELECT ST_MaximumInscribedCircle( geom ) AS mic FROM data) AS t ),
 shapes AS (
   SELECT svgShape( ST_ConvexHull( geom ),
     title => 'Convex Hull',
-    style => svgStyle('stroke', '#0088cc', 'stroke-width', 1::text,
-        'stroke-linejoin', 'round',
-        'fill', '#88ccff' ) )
+    style => svgStyle('stroke', '#0088cc', 'stroke-width', '1', 'fill', '#88ccff' ) )
     svg FROM data
-  UNION ALL
-  SELECT svgShape( ST_ConcaveHull( geom, 0.99 ),
+  UNION ALL SELECT svgShape( ST_ConcaveHull( geom, 0.99 ),
     title => 'Concave Hull',
-    style => svgStyle('stroke', '#0000ff', 'stroke-width', 1::text,
-        'stroke-opacity', 0.5::text, 'stroke-linejoin', 'round',
-        'fill', '#a0a0ff' ) )
+    style => svgStyle('stroke', '#0000ff', 'stroke-width', '1', 'fill', '#a0a0ff' ) )
     svg FROM data
-  UNION ALL
-  SELECT svgShape( ST_Buffer( center, radius ),
+  UNION ALL SELECT svgShape( ST_Buffer((mic).center, (mic).radius),
     title => 'Maximum Inscribed Circle',
-    style => svgStyle('stroke', '#6600aa', 'stroke-width', 1::text,
-        'stroke-opacity', 0.5::text, 'stroke-linejoin', 'round',
-        'fill', '#dd90ff' ) )
-    svg FROM mic
-  UNION ALL
-  SELECT svgShape( geom, radius => 2,
+    style => svgStyle('stroke', '#6600aa', 'stroke-width', '1', 'fill', '#dd90ff' ) )
+    svg FROM (SELECT ST_MaximumInscribedCircle( geom ) AS mic FROM data) AS t
+  UNION ALL SELECT svgShape( geom, radius => 2,
     style => svgStyle( 'fill', '#ff0000'  ) )
     svg FROM data
 )
