b83a230409baab353ff6c2d4b0eeac1bca87bba9
Martin Davis <mtnclimb@gmail.com>
2021-11-08 14:20:01 -0800

    Improve hilbert SQL

--- demo/fractal/hilbert-curve-svg.sql
+++ demo/fractal/hilbert-curve-svg.sql
@@ -18,13 +18,12 @@ WITH RECURSIVE lsystem AS (
 path AS ( SELECT replace(replace(state, 'A', ''), 'B', '') AS moves
   FROM (SELECT state FROM lsystem ORDER BY iteration DESC LIMIT 1) st
 ),
-pts AS ( SELECT moves AS moves, 1 AS index, ' ' AS dir,
-      0 AS xp, 0 AS yp, 0 AS x, 0 AS y, 1 AS dx, 0 AS dy, 0 AS len
-    FROM path
+pts(moves, index, dir, xp, yp, x, y, dx, dy, len) AS (
+  SELECT moves, 1, ' ',  0, 0, 0, 0, 1, 0, 0 FROM path
   UNION ALL
   SELECT moves, index+1 AS index, substr(moves, index, 1) AS dir,
       x AS xp, y AS yp,
-      x + len*dx AS x, y + len*dy AS y,
+      x + dx*len AS x, y + dy*len AS y,
       CASE substr(moves, index, 1) WHEN '-' THEN -dy WHEN '+' THEN  dy ELSE dx END AS dx,
       CASE substr(moves, index, 1) WHEN '-' THEN  dx WHEN '+' THEN -dx ELSE dy END AS dy,
       CASE substr(moves, index, 1) WHEN 'F' THEN 1 ELSE 0 END AS len
