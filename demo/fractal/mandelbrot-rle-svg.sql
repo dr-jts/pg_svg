f67bd9a3f200d1d4245c8c143ea3d5a44fe52da9
Martin Davis <mtnclimb@gmail.com>
2021-11-08 18:16:24 -0800

    Improve mandel-rle SQL

--- demo/fractal/mandelbrot-rle-svg.sql
+++ demo/fractal/mandelbrot-rle-svg.sql
@@ -5,49 +5,46 @@
 --================================================
 
 WITH RECURSIVE
-x(i) AS (   VALUES(0)
-            UNION ALL
-            SELECT i + 1 FROM x WHERE i < 401
-    ),
-z(ix, iy, cx, cy, x, y, i) AS (
+x(i) AS ( SELECT i FROM generate_series(0, 400) AS t(i) ),
+z(ix, iy, cx, cy, x, y, iter) AS (
     SELECT ix, iy, x::FLOAT, y::FLOAT, x::FLOAT, y::FLOAT, 0
       FROM        (SELECT -2.2 + 0.0074 * i, i FROM x) AS xgen(x, ix)
       CROSS JOIN  (SELECT -1.5 + 0.0074 * i, i FROM x) AS ygen(y, iy)
     UNION ALL
     SELECT ix, iy, cx, cy,
         x*x - y*y + cx AS x,
-        y*x*2 + cy AS y, i + 1
+        y*x*2 + cy AS y, iter + 1
     FROM z
     WHERE x*x + y*y < 16.0
-    AND i < 27
+    AND iter < 27
     ),
-itermax (ix, iy, i) AS (
-    SELECT ix, iy, MAX(i) AS i
+itermax (ix, iy, iter) AS (
+    SELECT ix, iy, MAX(iter) AS iter
     FROM z GROUP BY iy, ix
     ),
 runstart AS (
-    SELECT iy, ix, I,
-    CASE WHEN I = LAG(I) OVER (PARTITION BY iy ORDER By ix)
+    SELECT iy, ix, iter,
+    CASE WHEN iter = LAG(iter) OVER (PARTITION BY iy ORDER By ix)
         THEN 0 ELSE 1 END AS runstart
     FROM itermax
     ),
 runid AS (
-    SELECT iy, ix, I,
+    SELECT iy, ix, iter,
         SUM(runstart) OVER (PARTITION BY iy ORDER By ix) AS run
     FROM runstart
     ),
 rungroup AS (
-    SELECT iy, MIN(ix) ix, MAX(ix) ixend, MIN(i) i
+    SELECT iy, MIN(ix) ix, MAX(ix) ixend, MIN(iter) iter
     FROM runid
     GROUP BY iy, run
     ),
-plot(iy, ix, ixend, i, b, g) AS (
-    SELECT iy, ix, ixend, i,
-    CASE    WHEN i < 18 THEN (255 * i / 18.0 )::integer
-            WHEN i < 27 THEN 255
+plot(iy, ix, ixend, iter, b, g) AS (
+    SELECT iy, ix, ixend, iter,
+    CASE    WHEN iter < 18 THEN (255 * iter / 18.0 )::integer
+            WHEN iter < 27 THEN 255
             ELSE 0 END AS b,
-    CASE    WHEN i < 18 THEN 0
-            WHEN i < 27 THEN (255 * (i - 18) / (27 - 18 ))::integer
+    CASE    WHEN iter < 18 THEN 0
+            WHEN iter < 27 THEN (255 * (iter - 18) / (27 - 18 ))::integer
             ELSE 0 END AS g
     FROM rungroup ORDER BY iy, ix
 ),
