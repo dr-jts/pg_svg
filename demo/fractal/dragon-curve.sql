7e66500cc712e93dd29369bd0452e321c903e888
Martin Davis <mtnclimb@gmail.com>
2021-11-10 11:02:54 -0800

    Improve dragon SQL

--- demo/fractal/dragon-curve.sql
+++ demo/fractal/dragon-curve.sql
@@ -1,24 +1,25 @@
--- ---------------------------------
--- Dragon curve, as WKT
+-----------------------------------
+-- Dragon curve, as SVG
 -- Parameter: iter - # of iterations
--- ---------------------------------
 
 -- psql -A -t -o dragon-curve.svg  < dragon-curve.sql
+-----------------------------------
 
 WITH RECURSIVE
 lsystem( iter, state ) AS (
-  SELECT 0 AS iter, 'FX' AS state
+  SELECT 0 AS iter, 'FA' AS state
   UNION ALL
   SELECT iter + 1 AS iter,
-    replace(replace(replace(state, 'X', 'X+zF+'), 'Y', '-FX-Y'), 'z', 'Y') AS state
-  FROM lsystem WHERE iter < 10
+    replace(replace(replace(state, 'A', 'A+zF+'), 'B', '-FA-B'), 'z', 'B') AS state
+  FROM lsystem WHERE iter < 10      -- Parameter: iter
 ),
 path( moves ) AS (
-  SELECT replace(replace(state, 'A', ''), 'B', '') AS moves
+  SELECT replace(replace(state, 'A', ''), 'B', '')
   FROM (SELECT state FROM lsystem ORDER BY iter DESC LIMIT 1) st
 ),
 pts( moves, index, dir, x, y, dx, dy, len ) AS (
-  SELECT moves AS moves, 1 AS index, ' ' AS dir, 0 AS x, 0 AS y, 1 AS dx, 0 AS dy, 0 AS len  from path
+  SELECT moves AS moves, 1 AS index, ' ' AS dir, 0 AS x, 0 AS y, 1 AS dx, 0 AS dy, 0 AS len
+    FROM path
   UNION ALL
   SELECT moves, index+1 AS index, substr(moves, index, 1) AS dir,
       x + len*dx AS x,
@@ -31,13 +32,11 @@ pts( moves, index, dir, x, y, dx, dy, len ) AS (
 dragon(geom) AS (
   SELECT ST_RemoveRepeatedPoints( ST_MakeLine( ST_Point( x, y ) ORDER BY index ) ) FROM pts
 ),
-svg AS ( SELECT geom, svgShape( geom,
-        style => svgStyle('stroke', '#0000ff')
-      ) AS svg
+svg(geom, svg) AS (
+  SELECT geom, svgShape( geom, style => svgStyle('stroke', '#0000ff') )
     FROM dragon
 )
 SELECT svgDoc( array_agg( svg ),
           viewbox => svgViewbox( ST_Expand( ST_Extent(geom), 5 )),
-          style => svgStyle('stroke-width', '0.3', 'stroke-linecap', 'round' )
-    ) AS svg
+          style => svgStyle('stroke-width', '0.2', 'stroke-linecap', 'round' ) )
   FROM svg;
